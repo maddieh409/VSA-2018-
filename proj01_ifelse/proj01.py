fdd10bb99ae080fbbfd78ed1e31f86159c4bea80
maddieh409 <40777350+maddieh409@users.noreply.github.com>
2018-07-09 13:34:01 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -6,10 +6,49 @@
 # Part I:
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
+#
+# # Part II:
+# # This program asks the user for his/her name and birth month.
+# # Then, it prints a sentence that says the number of days and months until their birthday
+#
+#
+# # If you complete extensions, describe your extensions here!
+# print "Hello world!" + "cat"
+# print 5 + 3
+# #var name           input from user
+# user_age = raw_input("Enter your age: ")
+# print "you are " + user_age + " years old"
+# print int(user_age) +10
+# #var name              input from user
+# user_name = raw_input("Name: ")
+# user_grade = raw_input("What is your grade? ")
+# print "Maddie, you will graduate in", 12 - int(user_grade), "year(s)"
+# user_birthmonth = raw_input("When is your birthmonth? ")
+# print int(user_birthmonth)
+# user_birthdate = raw_input ("When is your birthdate? ")
+# print user_birthmonth, user_birthdate = raw_input("When is your birthday? ")
 
-# Part II:
-# This program asks the user for his/her name and birth month.
-# Then, it prints a sentence that says the number of days and months until their birthday
+current_month = raw_input("What is the current month? ")
+current_day = raw_input("What is the current day? ")
+user_birthmonth = raw_input("When is your birthmonth? ")
+user_birthdate = raw_input ("When is your birthdate? ")
 
+if user_birthmonth > 7:
+    print int(user_birthmonth) - 7
+elif user_birthmonth == 7:
+    print int(user_birthmonth) - 7
+else:
+    print int(user_birthmonth) + 5
 
-# If you complete extensions, describe your extensions here!
+# if user_birthmonth == 2:
+#     print (28 - int(user_birthdate)) + 9
+# if user_birthmonth == 9 or user_birthmonth == 4 or user_birthmonth == 6 or user_birthmonth == 11:
+#     print (30 - int(user_birthdate)) + 9
+# else:
+#     print (31 - int(user_birthdate)) + 9
+
+if user_birthdate > 9:
+    print int(user_birthdate) - 9
+if user_birthdate == 9:
+    print 0
+
