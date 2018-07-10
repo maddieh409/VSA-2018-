cca83f186c0c6662ad42b7ccad734299e85f6843
maddieh409 <40777350+maddieh409@users.noreply.github.com>
2018-07-10 10:21:50 -0500

    Add files via upload

---
+++ proj02_01.py
@@ -0,0 +1,49 @@
+# Name:
+# Date:
+
+# proj02: sum
+
+# Write a program that prompts the user to enter numbers, one per line,
+# ending with a line containing 0, and keep a running sum of the numbers.
+# Only print out the sum after all the numbers are entered
+# (at least in your final version). Each time you read in a number,
+# you can immediately use it for your sum,
+# and then be done with the number just entered.
+
+#Example:
+# Enter a number to sum, or 0 to indicate you are finished: 4
+# Enter a number to sum, or 0 to indicate you are finished: 5
+# Enter a number to sum, or 0 to indicate you are finished: 2
+# Enter a number to sum, or 0 to indicate you are finished: 10
+# Enter a number to sum, or 0 to indicate you are finished: 0
+# The sum of your numbers is: 21
+#
+# counter = 0
+# s = ""
+#
+# # key word while
+# while counter <= 10:
+#     print counter
+#     counter = counter + 1
+#     s = s + "a"
+#     print s
+#     if counter == 5:
+#         break
+#
+# sum = 0
+# user_input = 1
+# while user_input > 0:
+#     user_input = int(raw_input("Enter a number to sum"))
+#     sum = int(user_input) + sum
+# print int(user_input) + sum, "is your sum"
+
+# kew word is for
+# loops is over a range of numbers
+
+s = "vsa"
+# loop over strings
+for letter in s:
+    print letter
+    if letter == "s":
+        print "this is an s"
+#
