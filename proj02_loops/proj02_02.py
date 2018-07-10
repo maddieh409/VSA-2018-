e4ab0887c75f494378dd21a523513c0c18d33b1e
maddieh409 <40777350+maddieh409@users.noreply.github.com>
2018-07-10 10:23:35 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -10,4 +10,29 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+#
+# previous_number = 1
+# current_number = 1
+# next_number = 2
+# num = raw_input("Enter number here")
+#
+# for num in range (0, 22):
+#     print current_number
+#     next_number = previous_number + current_number
+#     previous_number = current_number
+#     current_number = next_number
+
+previous_number = 1
+current_number = 1
+number_of_fibnums = int(raw_input("Enter number here"))
+
+for number_of_fibnums in range(number_of_fibnums):
+    print current_number
+    previous_number = current_number
+    current_number = previous_number + current_number
+
+
+
+
+
 
