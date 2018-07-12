5c5d84763ac63e8e08129636390702d1617b3b6c
maddieh409 <40777350+maddieh409@users.noreply.github.com>
2018-07-12 11:41:04 -0500

    Add files via upload

--- listsorter.py
+++ listsorter.py
@@ -1,14 +1,29 @@
-o_list = [7, 11, 5, 19, 1]
-s_list = []
-while len(o_list) > 0:
-    small = o_list[0]
-    number = 0
-    count = 0
-    for x in o_list:
-        if x < small:
-            small = x
-            number = count
-        count += 1
-    s_list.append(small)
-    o_list.remove(o_list[number])
+import time
+import random
+
+list_len = 1000
+lst = []
+total_total_time = 0
+
+x = 1000
+tatol = 0
+while x > 0:
+    tatol += x
+    x -= 1
+print tatol * 100 * 1000
+
+for i in range (100):
+    tStart = time.clock()
+    for num in range(list_len):
+        lst.append(random.randint(0, 100))
+    s_list = []
+    while len(lst) > 0:
+        small = min(lst)
+        s_list.append(small)
+        lst.remove(small)
+    tEnd = time.clock()
+    total_time = tEnd - tStart
+    total_total_time += total_time
+
 print s_list
+print "time =", total_total_time / 100
